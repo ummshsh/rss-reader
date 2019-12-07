69c670de12f715c499c0b555906623eede04f6e3
Evgeniy Voronyuk <evoronuck@gmail.com>
2019-12-07 14:21:30 +0400

    Extension method added to convert com.proof.rssparser.Article to ArticleDatabase

--- app/src/main/java/com/ummshsh/rssreader/network/RssFetcher.kt
+++ app/src/main/java/com/ummshsh/rssreader/network/RssFetcher.kt
@@ -2,10 +2,24 @@ package com.ummshsh.rssreader.network
 
 import com.prof.rssparser.Article
 import com.prof.rssparser.Parser
+import com.ummshsh.rssreader.database.ArticleDatabase
 
 class RssFetcher {
-    suspend fun fetchEntries(url: String): MutableList<Article> {
+    suspend fun fetchEntries(url: String): List<Article> {
         val parser = Parser()
         return parser.getArticles(url)
+    }
+}
+
+fun List<Article>.asDatabaseArticle(): List<ArticleDatabase> {
+    return map {
+        ArticleDatabase(
+            -1,
+            it.guid.toString(),
+            -1,
+            it.title.toString(),
+            it.title.toString(),
+            it.description.toString()
+        )
     }
 }

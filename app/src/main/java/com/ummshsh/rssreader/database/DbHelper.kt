101b7983288366c17393459be263c957d56b5cfe
Evgeniy Voronyuk <evoronuck@gmail.com>
2020-04-14 21:35:56 +0400

    DbHelper.kt: atricle table query was added

--- app/src/main/java/com/ummshsh/rssreader/database/DbHelper.kt
+++ app/src/main/java/com/ummshsh/rssreader/database/DbHelper.kt
@@ -11,11 +11,20 @@ private const val SQL_CREATE_FEED_TABLE =
             "${DatabaseContract.Feed.COLUMN_NAME_TITLE} TEXT," +
             "${DatabaseContract.Feed.COLUMN_NAME_LINK} TEXT)"
 
+private const val SQL_CREATE_ARTICLE_TABLE =
+    "CREATE TABLE ${DatabaseContract.Article.TABLE_NAME} (" +
+            "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+            "${DatabaseContract.Article.COLUMN_NAME_FEED_ID} INTEGER," +
+            "FOREIGN KEY REFERENCES (${DatabaseContract.Article.COLUMN_NAME_FEED_ID}) REFERENCES ${DatabaseContract.Feed.TABLE_NAME}(${BaseColumns._ID})," +
+            "${DatabaseContract.Article.COLUMN_NAME_TITLE} TEXT," +
+            "${DatabaseContract.Article.COLUMN_NAME_DESCRIPTION} TEXT"
+
 private const val SQL_DELETE_FEED_TABLE = "DROP TABLE IF EXISTS ${DatabaseContract.Feed.TABLE_NAME}"
 
 class DbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase?) {
         db?.execSQL(SQL_CREATE_FEED_TABLE)
+        db?.execSQL(SQL_CREATE_ARTICLE_TABLE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {

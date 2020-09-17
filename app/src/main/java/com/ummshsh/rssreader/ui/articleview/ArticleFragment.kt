a47141d78e31fefd1a070e4d9afb200491e2645e
Evgeniy Voronyuk <evoronuck@gmail.com>
2020-09-17 14:44:16 +0400

    DataBinding was removed from ArticleFragment Cant add views prgrammatically because of that

--- app/src/main/java/com/ummshsh/rssreader/ui/articleview/ArticleFragment.kt
+++ app/src/main/java/com/ummshsh/rssreader/ui/articleview/ArticleFragment.kt
@@ -1,18 +1,13 @@
 package com.ummshsh.rssreader.ui.articleview
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import com.ummshsh.rssreader.R
-import com.ummshsh.rssreader.database.DatabaseContract
-import com.ummshsh.rssreader.databinding.ArticleFragmentBinding
-import com.ummshsh.rssreader.databinding.FeedManagementFragmentBinding
-import com.ummshsh.rssreader.ui.feedmanagement.FeedManagementViewModel
 
 class ArticleFragment : Fragment() {
 
@@ -21,15 +16,12 @@ class ArticleFragment : Fragment() {
     }
 
     private lateinit var viewModel: ArticleViewModel
-    val args: ArticleFragmentArgs by navArgs()
+    private val args: ArticleFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        var binding: ArticleFragmentBinding =
-            DataBindingUtil.inflate(inflater, R.layout.article_fragment, container, false)
 
         val activity = requireNotNull(this.activity) {
             "You can only access the viewModel after onActivityCreated()"
@@ -38,16 +30,8 @@ class ArticleFragment : Fragment() {
         viewModel = ViewModelProviders
             .of(this, ArticleViewModel.Factory(args.articleId, activity.application))
             .get(ArticleViewModel::class.java)
+        
 
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
-
-        return binding.root
+        return inflater.inflate(R.layout.article_fragment,container,false)
     }
-
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//        viewModel = ViewModelProviders.of(this).get(ArticleViewModel::class.java)
-//        // TODO: Use the ViewModel
-//    }
 }
